081f5d3e102ffaf63ab28ab603da803817771a9f
MIKE <mixa262@gmail.com>
2021-11-20 11:17:28 +0200

    filNotFoundExample

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,11 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,17 +15,33 @@ public class Main {
         School school = new School("TTU");
         Student abhita = new Student("Abi",23,"he009",school,"abc");
         //school.setStudents(new Student[]{abhita});
-        try{
-        school.getStudentbyID("he009");
-        }catch(NullPointerException e){
-           // System.out.println(e.getMessage());
-             e.printStackTrace();
-            //System.out.println("The error occurred - student not found");
-        }finally {
-            System.out.println("There was an error and the person could not be found in school - "+school.getName());
-        }
+//        try{
+//        school.getStudentbyID("he009");
+//        }catch(NullPointerException e){
+//           // System.out.println(e.getMessage());
+//             e.printStackTrace();
+//            //System.out.println("The error occurred - student not found");
+//        }finally {
+//            System.out.println("There was an error and the person could not be found in school - "+school.getName());
+//        }
 
+        //
+try{
+    sleepAndOpenFile("C:\\Users\\Mykhailo\\IdeaProjects\\StudentManagment\\Stude151ntManagment.iml");
+    System.out.println("The file was found");
+}catch (InterruptedException e){
+    System.out.println("Thread was disrupted - please run again");
+} catch(FileNotFoundException e){
+    System.out.println("FIle in question is not found!");
+}
 
+}
+    //new method for exceptions
+
+    public static void sleepAndOpenFile(String filePath)throws InterruptedException, FileNotFoundException {
+        Thread.sleep(2000);
+        new FileReader(filePath);
 
     }
+
 }
